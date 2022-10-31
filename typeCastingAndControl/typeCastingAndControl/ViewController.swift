de455b8be60b70aa986edd1b5fcb5f55830200e6
UlianaPereverzeva <115875780+UlianaPereverzeva@users.noreply.github.com>
2022-10-31 18:09:33 +0300

    collection creation

--- typeCastingAndControl/typeCastingAndControl/ViewController.swift
+++ typeCastingAndControl/typeCastingAndControl/ViewController.swift
@@ -11,9 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        //MARK:
+        // Создайте коллекцию типа [Any], включающую несколько вещественных чисел, целых, строк и булевых значений.  Распечатайте содержимое коллекции.
+        
+        
+        var arrayOfAnyType: [Any] = [2.21, 3.87, 4, 8, "Катя", "Ира", true, false]
+        print(arrayOfAnyType)
+        
+        
+        
     }
-
-
+    
 }
 
